ff1c7595dbca6879a4d44fe7994d5d7f0d2a5402
Omar Fahmy <Ofahmy1234@gmail.com>
2022-12-06 21:14:23 +0200

    feat: Add content field to tree node

--- src/utilities/treeNode.js
+++ src/utilities/treeNode.js
@@ -1,9 +1,17 @@
-module.exports = class treeNode{
-    constructor(value){
+ module.exports = class treeNode{
+    constructor(value ){
         this.value = value;
+        this.content = ""
         this.descendants = []
         this.parent = null
+        // this.id =id
+        this.id = Math.floor(Math.random()*100000000)
+        // this.id = Date.now()
     }
+    
+
 }
+
+
 
 
