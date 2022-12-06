4a1616be036586d438094a2a95bd0fa7d6f00152
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-12-06 18:25:56 +0200

    refactor: validation function
    refactor the code and convert it into module

--- src/features/validation.js
+++ src/features/validation.js
@@ -1,4 +1,3 @@
-const path = require("node:path");
 const readFile = require("../utilities/readFile");
 
 function getXMLTags(fileLine) {
@@ -46,24 +45,20 @@ function analyzeTags(allTags) {
 		const outerTag = allTags[outer];
 		for (let inner = outer + 1; inner < allTags.length; inner++) {
 			const innerTag = allTags[inner];
-			// check if both are opening tags.
-			if (outerTag.type === "opening" && innerTag.type === "opening") {
-				// if both have the same name then the first don't has corresponding closing tag.
-				if (getTagName(outerTag) === getTagName(innerTag)) {
-					// move to the next tag.
-					break;
-				}
-			}
-			// if one opening and the other closing.
-			else if (outerTag.type === "opening" && innerTag.type === "closing") {
-				// if both have the same name.
-				if (getTagName(outerTag) === getTagName(innerTag)) {
-					// if the closing hasn't matched before.
-					if (!innerTag.matched) {
-						// match both.
-						outerTag.matched = true;
-						innerTag.matched = true;
+			if (outerTag.type === "opening") {
+				if (innerTag.type === "opening") {
+					if (getTagName(outerTag) === getTagName(innerTag)) {
+						// outer don't has corresponding closing tag.
 						break;
+					}
+				} else if (innerTag.type === "closing") {
+					if (getTagName(outerTag) === getTagName(innerTag)) {
+						// if the closing hasn't matched before.
+						if (!innerTag.matched) {
+							outerTag.matched = true;
+							innerTag.matched = true;
+							break;
+						}
 					}
 				}
 			}
@@ -72,38 +67,42 @@ function analyzeTags(allTags) {
 }
 
 function getAnalysisFeedback(allTags) {
-	const filteredTags = allTags.filter(({ tag, matched }) => tag && !matched);
 	let feedback = [];
+	const filteredTags = allTags.filter(({ tag, matched }) => tag && !matched);
+
 	for (let index = 0; index < filteredTags.length; index++) {
 		const { tag, type, lineNumber } = filteredTags[index];
+
 		if (index + 1 < filteredTags.length) {
 			const nextTag = filteredTags[index + 1];
 			if (type === "opening" && nextTag.type === "closing") {
 				feedback.push(
-					`Error: tag ${tag} in line number ${lineNumber} doesn't match tag ${nextTag.tag} in ${lineNumber === nextTag.lineNumber ? "the same line" : `line number ${nextTag.lineNumber}`}.`
+					`Error: tag ${tag} in line number ${lineNumber} doesn't match tag ${nextTag.tag} ` +
+					`in ${ lineNumber === nextTag.lineNumber ? "the same line" : `line number ${nextTag.lineNumber}`}.`
 				);
 				index++;
+				continue;
 			}
-		} else if (type === "opening") {
+		}
+
+		if (type === "opening") {
 			feedback.push(`Error: tag ${tag} in line number ${lineNumber} has no corresponding closing tag.`);
-		} else if (type === "closing") {
+		}
+
+		if (type === "closing") {
 			feedback.push(`Error: tag ${tag} in line number ${lineNumber} has no corresponding opening tag.`);
 		}
+
 	}
 	return feedback;
 }
 
 // returns feedback array contains all errors found;
-async function validateXML(relativePath) {
-	const lines = await readFile(path.join(__dirname, relativePath));
+async function validateXML(absolutePath) {
+	const lines = await readFile(absolutePath);
 	const allTags = getAllTags(lines);
 	analyzeTags(allTags);
 	return getAnalysisFeedback(allTags);
 }
 
-// test
-(async () => {
-	let feedback = await validateXML("../../test.xml");
-	console.log(`${feedback.length} error(s) found.`);
-	feedback.forEach((error) => console.log(error));
-})();
+module.exports = validateXML;
