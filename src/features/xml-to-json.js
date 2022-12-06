dcc90402e443b44138974b31c3a40bd99797c23f
Omar Fahmy <Ofahmy1234@gmail.com>
2022-12-06 21:29:12 +0200

    feat: Finalize the creation of the json from xml

--- src/features/xml-to-json.js
+++ src/features/xml-to-json.js
@@ -12,7 +12,7 @@ const  getNode  = require('../utilities/searchTree.js');
 async function main() {
     let testPath = "D:/Kollya/Senior/Senior 1/term awl/Data Structure/project/XML-Analyzer/src/features/test.xml";
     let samplePath ="D:/Kollya/Senior/Senior 1/term awl/Data Structure/project/XML-Analyzer/sample.xml";
-    const file =  await readFile(testPath);
+    const file =  await readFile(samplePath);
  
     converToJSON(file);
 
@@ -22,19 +22,19 @@ async function main() {
 function converToJSON (xmlFile){
     let openingRegex = /<[a-zA-z0-9]+>/ ;
     let closingRegex = /<\/[a-zA-z0-9]+>/;
-    let counter = -1;
-    var openedTag = 1;
-    var closedTag =2;
-    var currentRoot=null , oldRoot = [];
-    var flag =-1
-    var root
-
+    // First traversion flag
+    let flag =-1
+    // Tree root
+    let root
+    
     let stack = new Stack();
-
-
+    // Current tree node scope
     let tagNode;
 
     xmlFile.forEach((line) => {
+        let content = line
+        let recentNode = ""
+
         if(openingRegex.test(line)){
             // console.log("OPENING => ",line);
             let openTagName = line.match(openingRegex)[0].replace(/[><]/g,"")
@@ -51,52 +51,47 @@ function converToJSON (xmlFile){
                 tagNode=temp2
                 console.log(root);
             }
-            console.log('opened tag =>',openedTag);
-            console.log('closed tag =>',closedTag);
-
-            // console.log(openTagName);
-            openedTag = openTagName
-            counter++;
-            //console.log(root);
+            content = content.replace(openingRegex,"")
             stack.push(openTagName)
         }
+
+
+
+
         if(closingRegex.test(line)){
-            // let temp2 = line.replace(/\// , "");
-            // console.log(stack.peek().match(openingRegex)[0]);
-            // console.log(line.match(closingRegex)[0].replace(/\// , ""));
-            var closeTagName = line.match(closingRegex)[0].replace(/[<>\/]/g , "")
-            // console.log('test =>',closeTagName);
-            if(stack.peek().match(closeTagName) == closeTagName ){
-                closedTag = closeTagName;
+
+            // var closeTagName = line.match(closingRegex)[0].replace(/[<>\/]/g , "")
+            content =  content.replace(closingRegex, "")
+            // if(stack.peek().match(closeTagName) == closeTagName ){
                 let temp = getNode(root , tagNode.parent)
+                recentNode = tagNode
                 tagNode = temp
 
-                // console.log("CLOSING => " , temp2);
                stack.pop()
 
-            }
+            //}
 
         }
 
+        //Checking if content is empty in certain line (and whether that line has a closing statement or not to use recent node instead of current node)
+        if(content.length != 0 && (closingRegex.test(line)===false)){
+            if(content[0] == ' ') content[0].trim()
+            console.log("HERE1",content);
+            tagNode.content += content
+        }else if (content.length != 0 && (closingRegex.test(line)===true)){
+            if(content[0] == ' ') content[0].trim()
+            console.log("HERE2",content);
+            recentNode.content += content
+        }
         
     })
 
-    console.log(stack.isEmpty());
-    console.log(root);
+
+
 
 
 }
 
 
-
+// Made like that because await can only be used inside async Functions
  main();
-
-
-
-
-                // / if(openedTag === closedTag ) {
-                //     let temp =new treeNode(closeTagName)
-                //    // if(tagNode.parent != null)
-                //    temp.parent = tagNode.parent
-                //      getNode(root,tagNode.parent).descendants.push(temp)
-                //      tagNode = temp
