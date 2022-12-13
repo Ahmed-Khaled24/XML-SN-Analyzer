7929d07c16a9173b1309d12931e7b7d933ded106
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-12-13 21:47:32 +0200

    feat: change openFile command response channel

--- src/electron/main-process/Handlers/openFile.handler.js
+++ src/electron/main-process/Handlers/openFile.handler.js
@@ -1,15 +1,15 @@
 const {readFileElectron} = require('../../../utilities/readFile');
 const {dialog} = require('electron');
 
-async function openFileHandler(event) {
-	const { canceled, filePaths } = await dialog.showOpenDialog({
+function openFileHandler(event) {
+	const { canceled, filePaths } = dialog.showOpenDialog({
 		title: "Open XML file",
 		filters: [{ name: 'Text files', extensions: ["xml"] }],
 	});
 	if (!canceled) {
 		let lines = readFileElectron(filePaths[0]);
 		const fileText = lines.join("\n");
-        event.sender.send('commandResponse', fileText)
+        event.sender.send('openFileResponse', fileText)
 	}
 }
 
