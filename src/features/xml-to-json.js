e6aabca04ebdcea7557ac2bb0fba6d7b10153c21
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-29 14:50:54 +0200

    feat: Add conversion from xml to compact json & change converter interface.

--- src/features/xml-to-json.js
+++ src/features/xml-to-json.js
@@ -3,10 +3,89 @@ const {treeNode} = require('../utilities/treeNode.js');
 const {Tree} = require('../utilities/Tree')
 
 
+function nonCompactJSON (xmlFile ,spacing){
+    let openingRegex = /<[a-zA-z0-9'"=\s-]+>/ ;
+    let closingRegex = /<\/[a-zA-z0-9]+>/;
+
+    // xml file wrapper
+    let obj ={};
+
+    // previous objects stack
+    let tags = [];
+    let root;
+
+    let content= "";
+    xmlFile.forEach((line) => {
+        line = line.trim()
+        content += line
+        if(openingRegex.test(line)){
+            let openTagName = line.match(openingRegex)[0].split(" ")[0].replace(/[><]/g,"").trim();
+            tags.push(openTagName)
+        }
+        content = content.replace(openingRegex,"").trim()
+        content = content.replace(closingRegex,"").trim();
 
 
 
-module.exports = function converToJSON (xmlFile){
+
+        if(closingRegex.test(line)){
+            let currentTag = tags[tags.length - 1];
+            let parentTag = tags[tags.length - 2];
+
+            if(tags.length === 1){
+                root = currentTag;
+            }else
+            if(content === "" && tags.length !== 1){
+                 if(typeof currentTag === "object" && typeof parentTag =="object"){
+                    let updatedEntry;
+                    let keys = Object.keys(parentTag);
+                    let lastKey = keys[keys.length - 1];
+                    if(Array.isArray(Object.entries(parentTag).pop()[1]) && Array.isArray(Object.entries(currentTag).pop()[1]) ){
+                        updatedEntry = {...parentTag , ...currentTag}
+                        tags[tags.length - 2] = updatedEntry
+                    }else if(Array.isArray(Object.entries(parentTag).pop()[1]) ){
+                        updatedEntry = tags[tags.length - 2][lastKey].push(currentTag)
+                        }else{
+                            updatedEntry = {...parentTag[lastKey] , ...currentTag}
+                        tags[tags.length - 2] = updatedEntry
+                    }
+
+                 }else if(!Array.isArray(parentTag)){
+                    tags[tags.length - 2] = {};
+                    tags[tags.length - 2][parentTag] = [];
+                    tags[tags.length - 2][parentTag].push(currentTag);
+                }else if(Array.isArray(parentTag)){
+                    tags[tags.length - 2][parentTag].push(currentTag);
+                }
+            }else{
+                if(typeof parentTag !== "object"){
+                    tags[tags.length - 2] = {[parentTag] : {}};
+               }
+               let keys = Object.keys(tags[tags.length - 2]);
+               let lastKey = keys[keys.length - 1];
+                if( tags[tags.length - 2][lastKey][currentTag] !== undefined){
+                    tags[tags.length - 2][lastKey][currentTag] += ", " + content;
+
+                }else{
+                    tags[tags.length - 2][lastKey][currentTag] = content;
+
+                }
+
+            }
+            content = ""
+            tags.pop();
+        }
+
+
+        
+    })
+
+    return JSON.stringify(root,null,spacing);
+
+}
+
+
+function compactJSON (xmlFile , spacing){
     let openingRegex = /<[a-zA-z0-9'"=\s-]+>/ ;
     let closingRegex = /<\/[a-zA-z0-9]+>/;
     // First opening tag flag
@@ -73,8 +152,12 @@ module.exports = function converToJSON (xmlFile){
     })
 
 
-    return (JSON.stringify(tree.root , null ,3)) 
+    return (JSON.stringify(tree.root , null ,spacing)) 
 }
 
+
+module.exports = function converToJSON(xmlFile , {compact , spacing}){
+    return compact === true? compactJSON(xmlFile,spacing) : nonCompactJSON(xmlFile,spacing);
+}
 
 
