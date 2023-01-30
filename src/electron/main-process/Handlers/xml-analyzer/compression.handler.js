d46da8eec5267e0dc312ddb9457079ff8f8e8370
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-30 23:52:34 +0200

    fix: remove redundant try catch block in compression handler

--- src/electron/main-process/Handlers/xml-analyzer/compression.handler.js
+++ src/electron/main-process/Handlers/xml-analyzer/compression.handler.js
@@ -3,15 +3,10 @@ const {huffManCompress , huffManDecompress} = require('../../../../features/xml-
 const minify = require('../../../../features/xml-analyzer/minify');
 
 function compressionHandler(event, data) {
-    try{
         data = data.split("\n")
         const fileMin = minify(data);
         const compressedFile= huffManCompress(fileMin);
         event.sender.send('compressResponse', compressedFile);
-    }catch(err){
-        dialog.showErrorBox('Error' , 'Cannot compress a minified file')
-    }
-
 }
 
 module.exports = compressionHandler;
