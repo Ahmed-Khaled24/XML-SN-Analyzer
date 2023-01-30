359f88a7287b1010af0d0d1ddd8966b54894dd21
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-30 11:20:22 +0200

    fix: remove debugging comments

--- src/electron/main-process/Handlers/visualization.handler.js
+++ src/electron/main-process/Handlers/visualization.handler.js
@@ -10,18 +10,6 @@ function visualizationHandler(event , data){
   const graphAdjList = CreateGraphAdjList(json);
   const graphData = formatGraph(graphAdjList.outList)
 
-    // let graph = {
-    //     nodes: [
-    //       { id: "n0", label: "A node", x: Math.floor(Math.random() * 1000) + 1,y:Math.floor(Math.random() * 1000) + 1 , size: 20 },
-    //       { id: "n1", label: "Another node", x: Math.floor(Math.random() * 1000) + 1, y:Math.floor(Math.random() * 1000) + 1  ,size: 20 },
-    //       { id: "n2", label: "And a last one", x: Math.floor(Math.random() * 1000) + 1, y:Math.floor(Math.random() * 1000) + 1 , size: 20 }
-    //     ],
-    //     edges: [
-    //       { id: "e0", source: "n0", target: "n1", type:'arrow', size:10 },
-    //       { id: "e1", source: "n1", target: "n2", type:'arrow', size:10},
-    //       { id: "e2", source: "n2", target: "n0", type:'arrow', size:10}
-    //     ]
-    //   }
   event.sender.send('visualizeReponse',JSON.stringify(graphData));
 
 }
