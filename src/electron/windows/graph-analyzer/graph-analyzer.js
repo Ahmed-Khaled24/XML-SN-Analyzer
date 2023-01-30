7a42468f992283a5bc1a5270805f355dbe350c7f
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-30 11:21:14 +0200

    feat: Add the get most connected user command to the second window

--- src/electron/windows/graph-analyzer/graph-analyzer.js
+++ src/electron/windows/graph-analyzer/graph-analyzer.js
@@ -1,27 +1,37 @@
 const {ipcRenderer} = require('electron');
 
+const mostActiveBtn = document.querySelector('.mostActiveUser-btn');
+const outputTextArea = document.querySelector('.outPutTextArea')
+
+mostActiveBtn.addEventListener('click' ,(e)=>{
+  const xml = localStorage.getItem("textFieldValue");
+  ipcRenderer.send('command', 'mostActiveUser', xml)
+  ipcRenderer.on('mostActiveUserResponse' ,(event , data)=>{
+    console.log(data);
+    outputTextArea.value = data;
+  })
+})
+
 document.querySelector('.xml-btn').addEventListener('click', () => {
     ipcRenderer.send('command', 'gotoXMLAnalyzer');
 })
 
     document.addEventListener('DOMContentLoaded' , ()=>{
         const xml = localStorage.getItem("textFieldValue");
-        localStorage.clear();
-        console.log(xml);
         const s = new sigma({
             renderer: {
               container: 'graph-container',
               type: 'canvas'
             },
             settings: {
-              defaultNodeColor: '#333',
+              defaultNodeColor: '#FFF',
               maxNodeSize: 20,
               minArrowSize:15,
               edgeColor: 'default',
-              defaultEdgeColor: '#ccc'
+              defaultEdgeColor: '#FFF'
             }
           });
-              ipcRenderer.send('command' , 'visualize' ,xml)
+        ipcRenderer.send('command' , 'visualize' ,xml)
         ipcRenderer.on('visualizeReponse' , (event , data)=>{
             data = JSON.parse(data);
               s.graph.read(data)
