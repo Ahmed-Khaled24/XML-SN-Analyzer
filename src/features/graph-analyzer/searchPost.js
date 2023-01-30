68513498795f15d5129ac65d2845e57b9b3af956
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-30 20:55:41 +0200

    feat: Add functionality to search posts of users and return the posts that include the search term

---
+++ src/features/graph-analyzer/searchPost.js
@@ -0,0 +1,21 @@
+module.exports = function searchPosts(json, searchValue){
+    let result = {};
+    const graphObj = JSON.parse(json);
+
+    graphObj.users.forEach(user => {
+        user.posts.forEach(post =>{
+            if(post.body.includes(searchValue)){
+                if(result[user.id] === undefined){
+                    result[user.id] = [];
+                    result[user.id].push(post.body);
+                }else{
+                    result[user.id].push(post.body);
+
+                }
+            }
+        })
+    });
+
+    return result;
+
+}
