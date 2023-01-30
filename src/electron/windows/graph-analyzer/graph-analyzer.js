5afd3276dfeb93e23f4c14d227c319ea08de7b80
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-30 09:55:38 +0200

    feat: Add functionality that loads up the visualization of the graph once the second window is loaded

--- src/electron/windows/graph-analyzer/graph-analyzer.js
+++ src/electron/windows/graph-analyzer/graph-analyzer.js
@@ -1,4 +1,30 @@
 const {ipcRenderer} = require('electron');
+
 document.querySelector('.xml-btn').addEventListener('click', () => {
     ipcRenderer.send('command', 'gotoXMLAnalyzer');
 })
+
+    document.addEventListener('DOMContentLoaded' , ()=>{
+        const g = localStorage.getItem("textFieldValue");
+        localStorage.clear();
+        console.log(g);
+        const s = new sigma({
+            renderer: {
+              container: 'graph-container',
+              type: 'canvas'
+            },
+            settings: {
+              defaultNodeColor: '#333',
+              maxNodeSize: 20,
+              minArrowSize:15,
+              edgeColor: 'default',
+              defaultEdgeColor: '#ccc'
+            }
+          });
+              ipcRenderer.send('command' , 'visualize')
+        ipcRenderer.on('visualizeReponse' , (event , data)=>{
+            data = JSON.parse(data);
+              s.graph.read(data)
+              s.refresh()
+        })
+    })
