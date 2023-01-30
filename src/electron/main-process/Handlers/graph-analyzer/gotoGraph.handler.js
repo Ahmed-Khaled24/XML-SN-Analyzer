4b4775ff8704408396354a6443088631d8a00421
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-30 23:27:17 +0200

    fix: Add json global variable

--- src/electron/main-process/Handlers/graph-analyzer/gotoGraph.handler.js
+++ src/electron/main-process/Handlers/graph-analyzer/gotoGraph.handler.js
@@ -3,9 +3,10 @@ const prettify = require('../../../../features/xml-analyzer/prettify');
 const convertToJSON = require('../../../../features/xml-analyzer/xml-to-json');
 
 function gotoGraphHandler(data) {
-	return CreateGraphAdjList(
-		convertToJSON( prettify(data), { compact: true, spacing: 3 } )
-	);
+	let json = convertToJSON( prettify(data), { compact: true, spacing: 3 } );
+	let ALGraph = CreateGraphAdjList(json);
+
+	return{json , ALGraph}
 };
 
 module.exports = gotoGraphHandler;
