91b165a4fe2e1237435247016b02f0a40b1650bd
Malek345 <73738469+Malek345@users.noreply.github.com>
2023-01-31 21:19:30 +0200

    fix: Ad safe traversal operator

--- src/features/graph-analyzer/searchPost.js
+++ src/features/graph-analyzer/searchPost.js
@@ -2,9 +2,9 @@ module.exports = function searchPosts(json, searchValue){
     let result = {};
     const graphObj = JSON.parse(json);
 
-    graphObj.users.forEach(user => {
-        user.posts.forEach(post =>{
-            if(post.body.toLowerCase().includes(searchValue.toLowerCase())){
+    graphObj.users?.forEach(user => {
+        user.posts?.forEach(post =>{
+            if(post.body?.toLowerCase().includes(searchValue.toLowerCase())){
                 if(result[user.id] === undefined){
                     result[user.id] = [];
                 }
