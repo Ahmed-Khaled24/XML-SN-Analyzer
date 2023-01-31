aab0507ce65e5934c8b8e038508533eed7dcd84c
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-31 23:19:00 +0200

    feat: Adjust the formatter for cytoscape

--- src/features/graph-analyzer/visualizationFormatter.js
+++ src/features/graph-analyzer/visualizationFormatter.js
@@ -1,35 +1,17 @@
 module.exports = function formatGraph(graphOutList) {
-	let edges = [];
 	let nodes = [];
-	let count = 0;
+	let edges = [];
 
-	// Generate nodes
-	Object.keys(graphOutList).forEach((key) => {
-		let node = {
-			id: key,
-			label: `user(${key})`,
-			x: Math.floor(Math.random() * 1000) + 1,
-			y: Math.floor(Math.random() * 1000) + 1,
-			size: 20,
-		};
-		nodes.push(node);
-	});
-
-	// Generate edges
-	Object.keys(graphOutList).forEach((key) => {
-		graphOutList[key].forEach((el) => {
-			let edge = {
-				id: '' + count,
-				source: key,
-				target: el,
-				color: '#282c34',
-				type: 'arrow',
-				size: 10,
-			};
-			edges.push(edge);
-			count++;
+	Object.keys(graphOutList).forEach((userID) => {
+		// Generate nodes
+		nodes.push({data: {id: userID, label: userID}});
+		// Generate edges
+		graphOutList[userID].forEach((followee) => {
+			edges.push({data: {source: userID, target: followee}});
 		});
 	});
+
+
 
 	return { nodes, edges };
 };
