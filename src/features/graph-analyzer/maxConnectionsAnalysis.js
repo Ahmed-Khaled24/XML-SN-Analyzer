6f2c0ecbe9bee808e98eab3249bff6229e719fc1
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-31 22:58:24 +0200

    fix: fix most active user to be the one who follows the most

--- src/features/graph-analyzer/maxConnectionsAnalysis.js
+++ src/features/graph-analyzer/maxConnectionsAnalysis.js
@@ -4,7 +4,7 @@ module.exports = function getMostConnected(adjList){
     let maxConnections = -1;
 
     Object.keys(adjList.inList).forEach(key =>{
-        let entry = adjList.inList[key].length + adjList.outList[key].length;
+        let entry = adjList.outList[key].length;
         if(entry > maxConnections) {
             maxConnections= entry;
             mostConnected = key;
