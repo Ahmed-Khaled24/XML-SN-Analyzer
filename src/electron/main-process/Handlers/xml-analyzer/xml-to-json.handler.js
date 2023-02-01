8de95a2160175c77ae2479e9516a1298464ab4a6
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-01 15:47:00 +0200

    feat: validate xml before convert to json

--- src/electron/main-process/Handlers/xml-analyzer/xml-to-json.handler.js
+++ src/electron/main-process/Handlers/xml-analyzer/xml-to-json.handler.js
@@ -1,10 +1,20 @@
-const convertToJSON = require('../../../../features/xml-analyzer/xml-to-json')
-const prettify = require('../../../../features/xml-analyzer/prettify')
+const convertToJSON = require('../../../../features/xml-analyzer/xml-to-json');
+const prettify = require('../../../../features/xml-analyzer/prettify');
+const validateXML = require('../../../../features/xml-analyzer/validation');
+const { dialog } = require('electron');
 
 function xmlToJSONHandler(event, data) {
-    const prettifiedData =  prettify(data);
-    const json = convertToJSON(prettifiedData ,{compact:true , spacing:3});
-    event.sender.send('xmlToJSONResponse', json);
+	const { valid } = validateXML(data.split('\n'), false);
+	if (!valid) {
+		dialog.showErrorBox(
+			'Invalid operation',
+			'Invalid XML data \nuse correct button then try again'
+		);
+		return;
+	}
+	const prettifiedData = prettify(data);
+	const json = convertToJSON(prettifiedData, { compact: true, spacing: 3 });
+	event.sender.send('xmlToJSONResponse', json);
 }
 
 module.exports = xmlToJSONHandler;
