e0e35bcb55390b4ce33a1504cacff4e7a8ebfadc
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-01 18:02:34 +0200

    feat: change openFileHandler
    modify openFileHandler to open xml files and compressed files

--- src/electron/main-process/Handlers/xml-analyzer/openFile.handler.js
+++ src/electron/main-process/Handlers/xml-analyzer/openFile.handler.js
@@ -2,15 +2,24 @@ const {readFileElectron} = require('../../../../utilities/readFile');
 const {dialog} = require('electron');
 
 async function openFileHandler(menuItem, browserWindow) {
-	const { canceled, filePaths } = await dialog.showOpenDialog({
-		title: "Open XML file",
-		filters: [{ name: 'Text files', extensions: ["xml"] }],
-	});
-	if (!canceled) {
-		let lines = readFileElectron(filePaths[0]);
-		const fileText = lines.join("\n");
-        browserWindow.webContents.send('openFileResponse', fileText);
+	let dialogResult;
+
+	if(menuItem.label === 'XML file'){
+		dialogResult = await dialog.showOpenDialog({
+			title: "Open XML file",
+			filters: [{ name: 'XML', extensions: ["xml"] }],
+		});
+	} else if( menuItem.label === 'Compressed file'){
+		dialogResult = await dialog.showOpenDialog({
+			title: "Open compressed file",
+			filters: [{ name: 'Compressed', extensions: ["tkf"] }],
+		});
 	}
+
+	if (dialogResult.canceled) return;
+	let lines = readFileElectron(dialogResult.filePaths[0]);
+	const fileText = lines.join("\n");
+	browserWindow.webContents.send('openFileResponse', fileText);
 }
 
 module.exports = openFileHandler;
