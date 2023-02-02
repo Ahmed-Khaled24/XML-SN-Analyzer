a76edfbe6780e36b4c7b7283367899e8aac63ca5
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-02 15:17:17 +0200

    fix: get data listeners outside button listeners

--- src/electron/windows/graph-analyzer/graph-analyzer.js
+++ src/electron/windows/graph-analyzer/graph-analyzer.js
@@ -2,7 +2,7 @@ const { ipcRenderer, autoUpdater } = require('electron');
 const zoomInBtn = document.querySelector('#zoomIn-btn');
 const zoomOutBtn = document.querySelector('#zoomOut-btn');
 const resizeBtn = document.querySelector('#resize-btn');
-const graphContainer = document.querySelector('#graph-container')
+const graphContainer = document.querySelector('#graph-container');
 const outputConsole = document.querySelector('.output-console textarea');
 const mostActiveBtn = document.querySelector('.most-active');
 const searchPostBtn = document.querySelector('.searchPost-btn');
@@ -11,67 +11,41 @@ const mostInfluencerBtn = document.querySelector('.most-influencer');
 const suggestBtn = document.querySelector('.suggest-friends button');
 const mutualFriendsBtn = document.querySelector('.mutual-friends button');
 // Visualization global variable
-let cyto; 
+let cyto;
 
 document.querySelector('.xml-btn').addEventListener('click', () => {
 	ipcRenderer.send('command', 'gotoXMLAnalyzer');
 });
-
 mostInfluencerBtn.addEventListener('click', () => {
 	ipcRenderer.send('command', 'getMostInfluencer');
-	ipcRenderer.on('mostInfluencerRes', (event, mostInfluencer) => {
-		outputConsole.value = `The most influencer is ${mostInfluencer}`;
-	});
 });
-
 mostActiveBtn.addEventListener('click', (e) => {
 	ipcRenderer.send('command', 'mostActiveUser');
-	ipcRenderer.on('mostActiveUserResponse', (event, mostActiveUser) => {
-		outputConsole.value = `The most active user is ${mostActiveUser}`;
-	});
 });
-
-searchPostBtn.addEventListener('click' , (e)=>{
-	if(!searchArea.value) {
+searchPostBtn.addEventListener('click', (e) => {
+	if (!searchArea.value) {
 		outputConsole.value = 'Error: search term input is empty';
 		return;
 	}
-	if(searchArea.value.trim() ==""){
-		outputConsole.value = "No such post exists"
+	if (searchArea.value.trim() == '') {
+		outputConsole.value = 'No such post exists';
 		return;
 	}
-	ipcRenderer.send('command', 'searchPosts' , searchArea.value.trim() );
-	ipcRenderer.on('searchPostsResponse' ,(event , data)=>{
-		console.log(data);
-		outputConsole.value = data;
-	})
-
-
-})
-
+	ipcRenderer.send('command', 'searchPosts', searchArea.value.trim());
+});
 suggestBtn.addEventListener('click', () => {
 	const userId = document.querySelector('.suggest-friends input').value;
 	if (!userId) return;
 	ipcRenderer.send('command', 'suggestFriends', userId);
-	ipcRenderer.on('suggestFriendsRes', (event, suggestResponse) => {
-		outputConsole.value = suggestResponse;
-	});
 });
-
 mutualFriendsBtn.addEventListener('click', () => {
 	const id1 = document.querySelector('.first-id').value;
 	const id2 = document.querySelector('.second-id').value;
 	if (!id1 || !id2) return;
 	ipcRenderer.send('command', 'getMutualFriends', [id1, id2]);
-	ipcRenderer.on('mutualFriendsRes', (event, mutualResponse) => {
-		outputConsole.value = mutualResponse;
-	})	
 });
 
-
 // Visualization occurs once page is loaded
-
-
 document.addEventListener('DOMContentLoaded', () => {
 	ipcRenderer.send('command', 'visualize');
 	ipcRenderer.on('visualizeRes', (event, graphData) => {
@@ -84,63 +58,80 @@ document.addEventListener('DOMContentLoaded', () => {
 					selector: '#graph-container',
 					style: {
 						height: '100vh !important',
-						width: '100vw !important'
-					  }
+						width: '100vw !important',
+					},
 				},
-			  {
-				selector: 'node',
-				style: {
-				  'background-color': '#e9ecef',
-				  label: 'data(label)',
-				  'text-valign': 'center',
-				  'text-halign': 'center'
-				}
-			  },
-			  {
-				selector: 'edge',
-				style: {
-				  'line-color': '#fcc419',
-				  'curve-style': 'bezier',
-				  'target-arrow-color': 'black',
-				  'target-arrow-fill': 'filled',
-				  'target-arrow-shape': 'triangle',
-				  'target-arrow-size': 40
-				}
-			  },
+				{
+					selector: 'node',
+					style: {
+						'background-color': '#e9ecef',
+						label: 'data(label)',
+						'text-valign': 'center',
+						'text-halign': 'center',
+					},
+				},
+				{
+					selector: 'edge',
+					style: {
+						'line-color': '#fcc419',
+						'curve-style': 'bezier',
+						'target-arrow-color': 'black',
+						'target-arrow-fill': 'filled',
+						'target-arrow-shape': 'triangle',
+						'target-arrow-size': 40,
+					},
+				},
 			],
 			layout: {
-				name: 'cose-bilkent',		  
-			  // idealEdgeLength:100
-			}
-		  });
+				name: 'cose-bilkent',
+				// idealEdgeLength:100
+			},
+		});
 	});
-
 });
-
-
-zoomInBtn.addEventListener('click' , ()=>{
+zoomInBtn.addEventListener('click', () => {
 	cyto.zoom({
 		level: cyto.zoom() * 1.5,
-		position: { x: graphContainer.offsetWidth / 2, y: graphContainer.offsetHeight / 2  }
-	});	
-
-})
-zoomOutBtn.addEventListener('click' , ()=>{
+		position: {
+			x: graphContainer.offsetWidth / 2,
+			y: graphContainer.offsetHeight / 2,
+		},
+	});
+});
+zoomOutBtn.addEventListener('click', () => {
 	cyto.zoom({
 		level: cyto.zoom() / 1.5,
-		position: { x: graphContainer.offsetWidth / 2, y: graphContainer.offsetHeight / 2  }
-	});	
-	
-})
-resizeBtn.addEventListener('click',(e)=>{
-	cyto.layout({name:'cose-bilkent'}).run();
+		position: {
+			x: graphContainer.offsetWidth / 2,
+			y: graphContainer.offsetHeight / 2,
+		},
+	});
+});
+resizeBtn.addEventListener('click', (e) => {
+	cyto.layout({ name: 'cose-bilkent' }).run();
 	// cyto.resize();
 	// cyto.fit();
-})
+});
+window.addEventListener('resize', () => {
+	container = cyto.container().children[0];
+	container.style.width = '100%';
+	container.style.height = '100%';
+});
 
-window.addEventListener('resize',  ()=> {
-	container = cyto.container().children[0]
-	container.style.width = "100%"
-	container.style.height="100%"
-  });
-  
+// Listeners
+ipcRenderer.on('mostInfluencerRes', (event, mostInfluencer) => {
+	outputConsole.value = `The most influencer is ${mostInfluencer}`;
+});
+ipcRenderer.on('mostActiveUserResponse', (event, mostActiveUser) => {
+	outputConsole.value = `The most active user is ${mostActiveUser}`;
+});
+ipcRenderer.on('searchPostsResponse', (event, data) => {
+	console.log(data);
+	outputConsole.value = data;
+});
+ipcRenderer.on('suggestFriendsRes', (event, suggestResponse) => {
+	outputConsole.value = suggestResponse;
+});
+ipcRenderer.on('mutualFriendsRes', (event, mutualResponse) => {
+	outputConsole.value = mutualResponse;
+});
