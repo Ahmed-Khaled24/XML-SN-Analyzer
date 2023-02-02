3f9db7d24d567aa35f91a480d56d797510b05e47
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-02 18:44:34 +0200

    feat: gotoGraph handler
    handle invalid data and show error with link for help

--- src/electron/main-process/Handlers/graph-analyzer/gotoGraph.handler.js
+++ src/electron/main-process/Handlers/graph-analyzer/gotoGraph.handler.js
@@ -1,12 +1,22 @@
 const CreateGraphAdjList = require('../../../../features/graph-analyzer/json-to-graph');
 const prettify = require('../../../../features/xml-analyzer/prettify');
 const convertToJSON = require('../../../../features/xml-analyzer/xml-to-json');
+const { errorWithLink } = require('../../../../utilities/showError');
 
 function gotoGraphHandler(data) {
-	let json = convertToJSON( prettify(data), { compact: true, spacing: 3 } );
-	let ALGraph = CreateGraphAdjList(json);
-
-	return{json , ALGraph}
-};
+	try {
+		let json = convertToJSON(prettify(data), { compact: true, spacing: 3 });
+		let ALGraph = CreateGraphAdjList(json);
+		return { json, ALGraph };
+	} catch (err) {
+		errorWithLink(
+			'Invalid data',
+			`Invalid Social Network Data
+		fix the data then try again`,
+			'https://github.com/Ahmed-Khaled24/XML-SN-Analyzer#how-to-install-and-build' // modify after write docs 
+		);
+		return {json: null, ALGraph: null};
+	}
+}
 
 module.exports = gotoGraphHandler;
