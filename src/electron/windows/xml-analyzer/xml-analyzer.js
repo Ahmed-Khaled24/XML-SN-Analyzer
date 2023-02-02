1821731d6491ae6b3ecafca818519cfd99f70665
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-02 16:26:56 +0200

    refactor: error handling in xml analyzer window

--- src/electron/windows/xml-analyzer/xml-analyzer.js
+++ src/electron/windows/xml-analyzer/xml-analyzer.js
@@ -14,102 +14,85 @@ const decompressBtn = document.querySelector('.decompress-btn');
 
 inputTextArea.focus();
 
-inputTextArea.addEventListener('keydown', (event) => {
-	if (event.keyCode == 9) {
-		event.preventDefault();
+inputTextArea.addEventListener('keydown', (e) => {
+	if (e.keyCode == 9) {
+		e.preventDefault();
 		inputTextArea.value += '    ';
 	}
 });
 
 validateBtn.addEventListener('click', (e) => {
-	if (!inputTextArea.value) {
-		ipcRenderer.send(
-			'error',
-			'There is no data to validate, insert xml data and try again.'
-		);
-		return;
-	}
-	ipcRenderer.send('command', 'validate', inputTextArea.value);
+	inputTextArea.value // Is there any data?
+		? ipcRenderer.send('command', 'validate', inputTextArea.value) // If there is data send it to main process to handle it
+		: sendError( // If there is no data return error
+				`There is no data to validate
+				insert xml data and try again`
+		);	
 });
 
 correctBtn.addEventListener('click', (e) => {
-	if (!inputTextArea.value) {
-		ipcRenderer.send(
-			'error',
-			'There is no data, insert xml data and try again.'
+	inputTextArea.value
+		? ipcRenderer.send('command', 'correct', inputTextArea.value)
+		: sendError(
+			`There is no data insert xml data and try again`
 		);
-		return;
-	}
-	ipcRenderer.send('command', 'correct', inputTextArea.value);
 });
 
 minifyBtn.addEventListener('click', (e) => {
-	if (!inputTextArea.value) {
-		ipcRenderer.send(
-			'error',
-			'There is no data to minify, insert xml data and try again.'
-		);
-		return;
-	}
-	ipcRenderer.send('command', 'minify', inputTextArea.value);
+	inputTextArea.value
+		? ipcRenderer.send('command', 'minify', inputTextArea.value)
+		: sendError(
+			`There is no data to minify 
+			insert xml data and try again`
+		);	
 });
 
 convertToJSONBtn.addEventListener('click', (e) => {
-	if (!inputTextArea.value) {
-		ipcRenderer.send(
-			'error',
-			'There is no data to convert, insert xml data and try again.'
+	inputTextArea.value
+		? ipcRenderer.send('command', 'convertToJSON', {
+				data: inputTextArea.value,
+				type: JSONTypeSelector.value,
+		  })
+		: sendError(
+				`There is no data to convert
+				insert xml data and try again`
 		);
-		return;
-	}
-	ipcRenderer.send('command', 'convertToJSON', {
-		data: inputTextArea.value,
-		type: JSONTypeSelector.value,
-	});
 });
 
 compressBtn.addEventListener('click', (e) => {
-	if (!inputTextArea.value) {
-		ipcRenderer.send(
-			'error',
-			'There is no data to compress, insert xml data and try again.'
-		);
-		return;
-	}
-	ipcRenderer.send('command', 'compress', inputTextArea.value);
+	inputTextArea.value
+		? ipcRenderer.send('command', 'compress', inputTextArea.value)
+		: sendError(
+				`There is no data to compress
+				insert xml data and try again`
+		);	
 });
 
 decompressBtn.addEventListener('click', (e) => {
-	if (!inputTextArea.value) {
-		ipcRenderer.send(
-			'error',
-			'There is no data to decompress, insert xml data and try again.'
+	inputTextArea.value
+		? ipcRenderer.send('command', 'decompress', outputConsole.value)
+		: sendError(
+				`There is no data to decompress
+				insert xml data and try again`
 		);
-		return;
-	}
-	ipcRenderer.send('command', 'decompress', outputConsole.value);
 });
 
 prettifyBtn.addEventListener('click', (e) => {
-	if (!inputTextArea.value) {
-		ipcRenderer.send(
-			'error',
-			'There is no data to prettify, insert xml data and try again.'
+	inputTextArea.value
+		? ipcRenderer.send('command', 'prettify', inputTextArea.value)
+		: sendError(
+				`There is no data to prettify
+				insert xml data and try again`
 		);
-		return;
-	}
-	ipcRenderer.send('command', 'prettify', inputTextArea.value);
 });
 
-graphBtn.addEventListener('click', () => {
-	if (!inputTextArea.value) {
-		ipcRenderer.send(
-			'error',
-			'There is no data to analyze in the graph, insert your xml data and try again.'
+graphBtn.addEventListener('click', (e) => {
+	inputTextArea.value
+		? ipcRenderer.send('command', 'gotoGraphWindow', inputTextArea.value)
+		: sendError(
+				`There is no data to analyze in the graph 
+				insert your xml data and try again`
 		);
-		return;
-	}
-	ipcRenderer.send('command', 'gotoGraphWindow', inputTextArea.value);
 });
 
 // Listeners
@@ -145,3 +128,8 @@ ipcRenderer.on('minifyResponse', (event, minifiedString) => {
 ipcRenderer.on('correctResponse', (event, lines) => {
 	inputTextArea.value = lines.join('\n');
 });
+
+
+function sendError(error) {
+	ipcRenderer.send('error', error);
+}
