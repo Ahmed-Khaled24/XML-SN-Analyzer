2375d387714b0a2ede0584f56a55d3d28f22cdc2
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-02 22:04:04 +0200

    fix: remove cytospace unused files

--- src/electron/windows/assets/lib/cytospace/cytoscape.js-3.23.0/benchmark/single/index.js
+++
@@ -1,5 +0,0 @@
-// set this to run just a single suite via `npm run benchmark:single`
-// (useful when working on a specific function)
-var suite = require('../collection-creation');
-
-suite.run({ async: true });
